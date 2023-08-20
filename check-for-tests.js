049174ebef5da332ca8c000d89b34dc51570c126
Joseph Dvorak <dvorakjt@gmail.com>
2023-08-20 16:47:34 -0400

    reduced package size by adding files array to package.json and setting minify to true in tsup.config.ts

---
+++ check-for-tests.js
@@ -0,0 +1,4 @@
+import fs from 'fs';
+if(!fs.existsSync('./src/__test__')) {
+  throw new Error('Test folder not included with published pacakge. To run tests, fork or clone the repo from https://github.com/dvorakjt/undecorated-di and run npm install. Then run npm test.');
+}
